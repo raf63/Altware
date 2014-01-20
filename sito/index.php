1eb64e09b3c9b2dccbe9cb70d5c4538e49edf441
Andrea Greco <andrea.greco.gapmilano@gmail.com>
2014-01-20 23:46:27 +0100

    Some fix at the projext and add Main

--- sito/index.php
+++ sito/index.php
@@ -7,15 +7,13 @@ include_once './HTML/LanguageDetect.php';
  * Title of page
  */
 $TITLE = ""; //Titolo
-
-
-
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <link href="Style/Style.css" rel="stylesheet" type="text/css">
         <link href="Style/Menu.css" rel="stylesheet" type="text/css">
+        <link href="Style/Main.css" rel="stylesheet" type="text/css">
         <meta charset="UTF-8">
         <title><?php echo $TITLE; ?></title>
     </head>
@@ -24,20 +22,52 @@ $TITLE = ""; //Titolo
 
             <div class="header">
                 <img id="Logo" src="./IMG/logo.png"/>
- 
-                <?php include_once './HTML/menu.php';?>
+
+<?php include_once './HTML/menu.php'; ?>
 
             </div>
 
             <div id="Main">
-                
-                
+                <table>
+                    <tr>
+                        <td><img src="IMG/pic18f4550.jpg"/></td>
+                        <td><img src="IMG/pic18f4550.jpg"/></td>
+                        <td><img src="IMG/pic18f4550.jpg"/></td>
+                        <td><img src="IMG/pic18f4550.jpg"/></td>
+                    </tr>
+                </table>
+
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdff</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdff</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfssddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfssddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfssddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfssddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfdfsdfsdfsdf</p>
+                <p>sddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+
+
+
             </div>
 
-            <div id="Foot">
-                <img src="./IMG/footbar.png"/> 
-                <?php echo $LANGUAGE_GENERAL_FOOT;?>     
-            </div>
+
+        </div>
+        <div id="Foot">
+            <img src="./IMG/footbar.png"/> <br>
+<?php echo $LANGUAGE_GENERAL_FOOT; ?>     
         </div>
     </body>
 </html>
