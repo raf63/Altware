ba627711765ba2767fbcefdb7a75c5ca6feaf09d
Andrea Greco <andrea.greco.gapmilano@gmail.com>
2014-01-22 00:01:17 +0100

    fixed foot ALTWARE.. in the footer of page.

--- sito/Lang/ITA.php
+++ sito/Lang/ITA.php
@@ -8,7 +8,7 @@ $LANGUAGE_GENERAL_MENU_HOME="Ritorna alla pagina principale";
 $LANGUAGE_GENERAL_MENU_TEAM="Scopri il team che ha creato tutto questo";
 $LANGUAGE_GENERAL_MENU_EMAIL="Contattaci per feedback e problemi";
 //FOOT
-$LANGUAGE_GENERAL_FOOT="Tutti i diritti riservati. ALTWARE@2013-2014";
+$LANGUAGE_GENERAL_FOOT="Tutti i diritti riservati.";
 
 
 
